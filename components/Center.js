477304676f9a03c793e5576a2e21111b5655c6af
diegomota2374 <diegomota2374@gmail.com>
2023-05-29 08:50:32 -0300

    realizado primeiro commit de configuração do front

---
+++ components/Center.js
@@ -0,0 +1,11 @@
+const { default: styled } = require("styled-components");
+
+const StyledDiv = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding:0 20px;
+`;
+
+export default function Center({ children }) {
+  return <StyledDiv>{children}</StyledDiv>;
+}
