3d6df05fee38e572e22585e634764489167059dd
diegomota2374 <diegomota2374@gmail.com>
2023-05-29 18:07:13 -0300

    foi configurado o header com o nav e a feature do product

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
+import Featured from "@/components/Featured";
 import Header from "@/components/Header";
+import { mongooseConnect } from "@/lib/mongoose";
+import { Product } from "@/models/Product";
 
-export default function HomePage() {
+export default function HomePage({ product }) {
   return (
     <div>
       <Header />
+      <Featured product={product} />
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const featuredProductId = "6462643db7f7294e1ee5339e";
+  await mongooseConnect();
+  const product = await Product.findById(featuredProductId);
+  return {
+    props: { product: JSON.parse(JSON.stringify(product)) },
+  };
 }
